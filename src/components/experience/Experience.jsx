24779f68b7617cafb69371f62824c8e3b6e27a8e
Ramzan Chandia <Ramxanchandia31@gmail.com>
2022-11-08 09:07:01 +0500

    commit 08/11/2022

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,116 @@
 import React from 'react'
 import './experience.css'
+import {BsFillPatchCheckFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience_container">
+
+        {/* FrontEnd */}
+        <div className="experience_frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience_content">
+            <article className="experience_details">
+              <BsFillPatchCheckFill className = "experience_details-icon" />
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience_details">
+              <BsFillPatchCheckFill className = "experience_details-icon" />
+              <div>
+                <h4>CSS</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+
+            <article className="experience_details">
+              <BsFillPatchCheckFill className = "experience_details-icon" />
+              <div>
+                <h4>JavaScript</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience_details">
+              <BsFillPatchCheckFill className = "experience_details-icon" />
+              <div>
+                <h4>Bootstrap</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+
+            <article className="experience_details">
+              <BsFillPatchCheckFill className = "experience_details-icon" />
+              <div>
+                <h4>React JS</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience_details">
+              <BsFillPatchCheckFill className = "experience_details-icon" />
+              <div>
+                <h4>WordPress</h4>
+                <small className='text-light'>Experienced</small> 
+              </div>
+            </article>
+          </div>
+        </div>
+
+        {/* BackEnd */}
+        <div className="experience_backend">
+        <h3>Backend Development</h3>
+          <div className="experience_content">
+            <article className="experience_details">
+              <BsFillPatchCheckFill className = "experience_details-icon" />
+              <div>
+                <h4>Node JS</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience_details">
+              <BsFillPatchCheckFill className = "experience_details-icon" />
+              <div>
+                <h4>Express JS</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience_details">
+              <BsFillPatchCheckFill className = "experience_details-icon" />
+              <div>
+                <h4>MongoDB</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience_details">
+              <BsFillPatchCheckFill className = "experience_details-icon" />
+              <div>
+                <h4>PHP</h4>
+                <small className='text-light'>Basic</small>
+              </div>
+            </article>
+
+            <article className="experience_details">
+              <BsFillPatchCheckFill className = "experience_details-icon" />
+              <div>
+                <h4>Python</h4>
+                <small className='text-light'>Basic</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -3,7 +3,35 @@ import './services.css'
 
 const Services = () => {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services_container">
+        <article className="service">
+          <div className="service_head">
+            <h3>Own A Website That Works</h3>
+          </div>
+          <p>If you have a dream, you also have a website. It has to look good and you want it to work each time. Well, that's why I am here. I will turn your ideas and dreams into reality. I will help you with designing, developing, and hosting a great site that will improve your business.</p>
+        </article>
+
+
+        <article className="service">
+          <div className="service_head">
+            <h3>Control The Features</h3>
+          </div>
+          <p>Do you feel restricted by all the drag-and-drop web builders out there? I offer a powerful custom code solution that fits your specific needs. Eventually, you will become the owner of a fast, handsome site. You will be able to change any existing functionality or can add new functionality in your website by hiring any web developer.</p>
+        </article>
+
+
+        <article className="service">
+          <div className="service_head">
+            <h3>Sell Your Stuff Online</h3>
+          </div>
+          <p>You have a vision. You want to make it an online business without any problems and restrictions. Perfect! I am here to help you set up an e-commerce store that looks amazing and makes you money as much as you want and bright your future as you wish.</p>
+        </article>
+      </div>
+    </section>
   )
 }
 
